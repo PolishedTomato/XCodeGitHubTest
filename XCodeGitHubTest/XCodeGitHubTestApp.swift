c4290e0e16e26b851964c9c67fd055a328184e5a
Deye Lei <Dleipersonal@gmail.com>
2023-05-15 01:16:21 -0400

    Initial Commit

---
+++ XCodeGitHubTest/XCodeGitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeGitHubTestApp.swift
+//  XCodeGitHubTest
+//
+//  Created by Deye Lei on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct XCodeGitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
